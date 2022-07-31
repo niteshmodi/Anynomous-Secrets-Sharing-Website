5b32fffbb9f99a2255790746f27dc7be9f6dd0a1
niteshmodi <89907820+niteshmodi@users.noreply.github.com>
2022-08-01 00:02:45 +0530

    added md5 hashing

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
-console.log(process.env.API_KEY);
+
 
 
 const app = express();
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"]}); 
+
 
 const User= new mongoose.model("User" , userSchema);
 
@@ -47,7 +47,7 @@ app.get("/register" , function(req , res){
 app.post("/register" , function (req , res){
      const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
      });
 
      newUser.save(function(err){
@@ -63,7 +63,7 @@ app.post("/register" , function (req , res){
 
 app.post("/login" , function( req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username} , function(err , foundUser){
        if(err){
